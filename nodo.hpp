b15e181c792855c73f859c4316df32c4e3e81ae5
Luis <luisdimeo16@gmail.com>
2026-07-15 15:35:55 -0400

    docs:archivo inicial datos.csv

--- nodo.hpp
+++ nodo.hpp
@@ -22,5 +22,21 @@ struct Node {
     Node* left;   // Primer sucesor
     Node* right;  // Segundo sucesor
     Node* parent; // Jefe directo (Padre en el árbol)
+    Node(int _id, std::string _name, std::string _last_name, char _gender, int _age, int _id_boss,
+         bool _is_dead, bool _in_jail, bool _was_boss, bool _is_boss) {
+        id = _id;
+        name = _name;
+        last_name = _last_name;
+        gender = _gender;
+        age = _age;
+        id_boss = _id_boss;
+        is_dead = _is_dead;
+        in_jail = _in_jail;
+        was_boss = _was_boss;
+        is_boss = _is_boss;
+        left = nullptr;
+        right = nullptr;
+        parent = nullptr;
+    }
 };
-
+#endif
